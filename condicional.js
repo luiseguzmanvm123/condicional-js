f59f4155c7a4ef1a054902213b14e5c3b502bffe
Luis Enrique <127609212+luiseguzmanvm123@users.noreply.github.com>
2023-03-11 08:50:24 -0600

    Add files via upload

---
+++ condicional.js
@@ -0,0 +1,38 @@
+/* let num1 = prompt("hola " + " dame un numero");
+if (num1 < 0) {
+    alert("negativo")
+}
+else { alert("positivo") } */
+/* let num1=prompt("hola "+" dame un numero");
+if(num1%2){
+    alert("inpar")
+}
+else{alert("par")} */
+/* let num1=prompt("dame un numero");
+let num2=prompt("dame tu segundo numero");
+if(num2>num1){
+    
+alert(num2 - num1)
+
+}
+else{alert(num1-num2)} */
+/* let num1=prompt("numero de articulos");
+let num2=prompt("precio");
+n=num1*num2;
+if(n>=40){
+    alert(n-(n*0.15))
+}
+else{alert(n)} */
+let nom1 = prompt("INTRODUCE TU NOMBRE ")
+if (nom1 == "maximiliano") {
+    alert("no puedes entrar con ese nombre")
+}
+else {
+    (nom1 = nom1 )
+
+    let cont = prompt("introduce tu contraseña ")
+    if (cont != "tommy") {
+        alert("esta incorrecto " + nom1)
+    }
+    else { alert("BIENVENIDO " +nom1) }
+}
